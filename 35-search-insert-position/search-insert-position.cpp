f4fef921daf798973657244455541c4dd4432269
anshul-dying <86553447+anshul-dying@users.noreply.github.com>
2025-02-04 23:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (40.57%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int s = 0;
+        int e = nums.size()-1;
+        int mid = s+(e-s)/2;
+
+        while(s<=e)
+        {
+            if(target == nums[mid])
+                return mid;
+            else if(target < nums[mid])
+            {
+                e = mid-1;
+            }
+            else
+            {
+                s = mid+1;
+            }
+            mid = s+(e-s)/2;
+            
+            // if(target == nums[mid])
+            //     return mid;
+            // else if(s==e && target > nums[mid])
+            //     return mid+1;
+            // else if(s==e && target < nums[mid])
+            //     return mid;
+        }
+
+        return s;
+    }
+};
